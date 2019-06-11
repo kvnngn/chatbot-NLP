e4c79a671e98959089fb3ac221a0796d498b8ff1
Nguyen Kévin <kevin3.nguyen@epitech.eu>
2019-06-11 19:00:40 +0900

    init repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { AppComponent } from "./app.component";
+import {
+  MessageListComponent,
+  MessageFormComponent,
+  MessageItemComponent
+} from "@app/components";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MessageListComponent,
+    MessageFormComponent,
+    MessageItemComponent
+  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
